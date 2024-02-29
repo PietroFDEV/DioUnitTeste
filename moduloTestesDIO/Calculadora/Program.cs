25d646536999b8788b478c0cfe6d0a2d069be442
PietroFDEV <pi_favoreto@yahoo.com.br>
2024-02-28 21:39:49 -0300

    Desafio codigo
    Desafio para ver media de notas de 0 a 10

--- moduloTestesDIO/Calculadora/Program.cs
+++ moduloTestesDIO/Calculadora/Program.cs
@@ -1,8 +1,12 @@
 ﻿using Calculadora.Services;
 
 CalculadoraImp c = new CalculadoraImp();
+//exec1
+// int num1 = 5;
+// int num2 = 10;
 
-int num1 = 5;
-int num2 = 10;
+// Console.WriteLine($"{num1} + {num2} = {c.Somar(num1, num2)}");
 
-Console.WriteLine($"{num1} + {num2} = {c.Somar(num1, num2)}");
+//exec2
+string[] teste = {"-3,5",  "3,5", "11,0", "10,0"};
+c.aaa(teste);
--- moduloTestesDIO/Calculadora/Services/CalculadoraImp.cs
+++ moduloTestesDIO/Calculadora/Services/CalculadoraImp.cs
@@ -16,5 +16,27 @@ namespace Calculadora.Services
         {
             return num % 2 == 0;
         }
+        public void aaa(string[] args) { 
+            //desafio de codigo
+        decimal media = 0;
+        int count = 0;
+        for (int index = 0; index < args.Length; index++)
+        {
+            decimal num = Convert.ToDecimal(args[index]);
+            if(num < 0 || num > 10)
+            {
+            Console.WriteLine("nota invalida");
+            }
+            else
+            {
+                media = num + media;
+                count++;
+            }
+            
+        }
+        decimal result = (decimal)media / count;
+
+        Console.WriteLine($"media = {result}");
+        }
     }
 }
